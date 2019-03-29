7f8e7d9e5d2f52a4fa6685aa2670ca42ddfc9aa2
TylerGoh <tylergoh51@gmail.com>
2019-03-29 21:44:50 +0800

    s

--- server.js
+++ server.js
@@ -5,7 +5,27 @@ const mongoose = require('mongoose');
 const cors = require('cors')
 const url = `mongodb://tyler:tyler@127.0.0.1:27017/coffeeshop?authSource=admin`;
 
+const accountSchema = mongoose.Schema({
+    _id: mongoose.Schema.Types.ObjectId,
+    name: String,
+    password: String
+})
+
+const Account = mongoose.model('Account', accountSchema);
+
+var test = new Product({
+    _id: new mongoose.Types.ObjectId(),
+    name: "test",
+    password: "pass"
+})
+
+test.save().then(result => {
+    console.log(result);
+})
+
 mongoose.connect(url, {useNewUrlParser: true});
+
+
 
 serv.listen(80);
 app.use(cors());
