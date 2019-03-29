6d1db8931c9c7780867cb7fa16887862250b6d45
TylerGoh <tylergoh51@gmail.com>
2019-03-29 18:31:27 +0800

    a

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const serv = require('http').Server(app);
 const mongoose = require('mongoose');
+const cors = require('cors')
 const url = `mongodb://tyler:tyler@127.0.0.1:27017/coffeeshop?authSource=admin`;
 
 mongoose.connect(url, {useNewUrlParser: true});
